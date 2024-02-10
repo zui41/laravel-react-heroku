190f0f2c5a660ae2f81ef67c026ed0152412604c
zui41 <zui19990401@gmail.com>
2024-02-10 23:36:37 +0900

    [add] added controller

---
+++ app/Http/Controllers/GroupController.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Models\Group;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use Exception;
+
+class GroupController extends Controller
+{
+    /**
+     * Show all group informations.
+     *
+     */
+    public function index(): Response
+    {
+        try {
+            $groups = Auth::user()->groups()->get();
+            // TODO Postの新しい順にソートしたい
+            // $groups = Auth::user()->groups()->orderBy()->get();
+
+            return response()->json($groups, 200);
+        } catch (Exception $e) {
+            Log::error($e);
+
+            return response()->json(404);
+        }
+    }
+
+    /**
+     * Store group information.
+     *
+     */
+    public function store(Request $request): Response
+    {
+        try {
+            $img_path = null;
+            if ($request->file('img')) {
+                $img_path = $request->file('img')->store('group_imgs');
+            }
+            $group = Group::create([
+                'name' => $request->name,
+                'img_path' => $img_path,
+            ]);
+
+            $user = Auth::user();
+            $user->groups()->sync($group->id, false);
+
+            return response()->json($group, 201);
+        } catch (Exception $e) {
+            DB::rollback();
+            Log::error($e);
+
+            return response()->json(404);
+        }
+    }
+
+    /**
+     * Show group detail.
+     *
+     */
+    public function detail(Request $request): Response
+    {
+        try {
+            $group = Group::find($request->id);
+
+            return response()->json($group, 200);
+        } catch (Exception $e) {
+            Log::error($e);
+
+            return response()->json(404);
+        }
+    }
+
+    /**
+     * Update group detail.
+     *
+     */
+    public function update(Request $request): Response
+    {
+        try {
+            $group = Group::find($request->id);
+            if ($request->file('img')) {
+                Storage::delete($group->img_path);
+                $img_path = $request->file('img')->store('group_imgs');
+            }
+            $group->update([
+                'name' => $request->name,
+                'img_url' => $img_path,
+            ]);
+
+            return response()->json($group, 200);
+        } catch (Exception $e) {
+            DB::rollback();
+            Log::error($e);
+
+            return response()->json(404);
+        }
+    }
+
+    /**
+     * Delete group information.
+     *
+     */
+    public function delete(Request $request): Response
+    {
+        try {
+            $group = Group::find($request->id);
+
+            $user = Auth::user();
+            $user->groups()->detach($group->id);
+
+            $group->delete();
+
+            return response()->json(null, 204);
+        } catch (Exception $e) {
+            DB::rollback();
+            Log::error($e);
+
+            return response()->json(404);
+        }
+    }
+}
---
+++ app/Http/Controllers/ThemeController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Theme;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class ThemeController extends Controller
+{
+    /**
+     * Show all theme informations.
+     *
+     */
+    public function index(Request $request): Response
+    {
+        try {
+            $themes = Theme::where('group_id', $request->group_id)
+                            ->orderBy('created_at')->get();
+            // TODO Postの新しい順にソートしたい
+            // $themes = Theme::where('group_id', $request->group_id)
+            //                 ->orderBy('created_at')->get();
+
+            return response()->json($themes, 200);
+        } catch (Exception $e) {
+            Log::error($e);
+
+            return response()->json(404);
+        }
+    }
+
+    /**
+     * Store theme information.
+     *
+     */
+    public function store(Request $request): Response
+    {
+        try {
+            $theme = Theme::create([
+                'group_id' => $request->group_id,
+                'name' => $request->name,
+            ]);
+
+            return response()->json($theme, 201);
+        } catch (Exception $e) {
+            DB::rollback();
+            Log::error($e);
+
+            return response()->json(404);
+        }
+    }
+
+    /**
+     * Show theme detail.
+     *
+     */
+    public function detail(Request $request): Response
+    {
+        try {
+            $theme = Theme::find($request->id);
+
+            return response()->json($theme, 200);
+        } catch (Exception $e) {
+            Log::error($e);
+
+            return response()->json(404);
+        }
+    }
+
+    /**
+     * Update theme information.
+     *
+     */
+    public function update(Request $request): Response
+    {
+        try {
+            $theme = Theme::find($request->id)->update([
+                'name' => $request->name,
+            ]);
+
+            return response()->json($theme, 200);
+        } catch (Exception $e) {
+            DB::rollback();
+            Log::error($e);
+
+            return response()->json(404);
+        }
+    }
+
+    /**
+     * Delete theme information.
+     *
+     */
+    public function delete(Request $request): Response
+    {
+        try {
+            Theme::find($request->id)->delete();
+
+            return response()->json(null, 204);
+        } catch (Exception $e) {
+            DB::rollback();
+            Log::error($e);
+
+            return response()->json(404);
+        }
+    }
+}
