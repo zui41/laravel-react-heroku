beb154986a797a6bb2cf4c82c1f779a2195b4ad5
F0gr1 <gami20010314@gmail.com>
2024-02-11 03:34:25 +0900

    update rooting arround

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,135 +1,151 @@
+import { useEffect , useState} from 'react';
+import Checkbox from '@/Components/Checkbox';
+import GuestLayout from '@/Layouts/GuestLayout';
+import InputError from '@/Components/InputError';
+import InputLabel from '@/Components/InputLabel';
+import PrimaryButton from '@/Components/PrimaryButton';
+import TextInput from '@/Components/TextInput';
+import { Head, Link, useForm } from '@inertiajs/react';
 import styled from 'styled-components';
-import { Link, Head } from '@inertiajs/react';
-import { Route, Routes } from 'react-router-dom';
-import { useEffect, useState } from 'react'; // useStateを追加
-import GroupComponent from '@/Components/Group/GroupComponent';
-import ThemeComponent from '@/Components/Theme/ThemeComponent';
-import ThreadComponent from '@/Components/Thread/ThreadComponent';
-import store from '@/Store/store';
+import axios from 'axios';
 import { setAuth } from '@/Store/authSlice';
 import { useDispatch } from 'react-redux';
+import { Inertia } from '@inertiajs/inertia';
 
-const GroupThemeContainer = styled.div`
-    width: 100%;
-    height: 85%;
-    display: flex;
-    flex-direction: row;
+
+
+// スタイルドコンポーネント
+const StatusMessage = styled.div`
+  margin-bottom: 1rem;
+  font-weight: 800; 
+  font-size: 0.9rem; 
+  color: #ffffcc; 
 `;
 
-const GroupContainer = styled.div`
-    width: 10%;
-    height: 100vh;
-    color: white;
-    background-color: #333;
+const LabelContainer = styled.label`
+  display: block;
+  margin-top: 1rem; 
+  margin-left: 5px;
 `;
 
-const ThemeContainer = styled.div`
-    width: 90%;
-    height: 100vh;
-    color: white;
-    overflow-y: auto;
-    margin: 0 3%;
+const LinkStyled = styled(Link)`
+  text-decoration: underline;
+  font-size: 0.9rem; 
+  color: #909090; 
+  &:hover {
+    color: black; /* hover:text-gray-900 */
+  }
+  margin-start: 0.5rem; /* ms-2 */
+  padding-right :10px
 `;
 
-const WelcomeContainer = styled.div`
-    min-height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
+const PassRem =styled.span`
+  mergin-left: 10px;
+`
+const ButtonContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  margin-top: 1rem;
+  
 `;
 
-const WelcomeContent = styled.div`
-    width: 100%;
-    height: 100%;
-    z-index: 1;
+const LoginForm = styled.form`
+  height: 100%;
 `;
 
-const WelcomeLink = styled(Link)`
-    font-weight: 600;
-    color: #4a5568;
-    &:hover {
-        color: #2d3748;
+// ページコンポーネント
+export default function Login({ status, canResetPassword }) {
+  const { data, setData, post, processing, errors, reset } = useForm({
+    email: '',
+    password: '',
+    remember: false,
+  });
+  const dispatch = useDispatch(); 
+  const [user, setUser] = useState({}); // useStateを使用
+  
+  useEffect(() => {
+    return () => {
+      reset('password');
+    };
+  }, []);
+
+  const submit = async(e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('/api/login', data);
+
+      if (!response.data) {
+        throw new Error('Invalid response format');
+      }
+      if (response.status !=201){
+        throw new Error('Error');
+      }  else{
+        setUser(response.data);
+        dispatch(setAuth(response.data));
+        Inertia.visit('/group/1');
+      }
+    } catch (error) {
+      console.error('Error:', error.message);
     }
-`;
+  };
 
-const Header = styled.div`
-    height: 15%;
-`;
+  return (
+    <GuestLayout>
+      <Head title="Log in" />
 
-export default function Welcome({ auth }) {
-    const dispatch = useDispatch();
-    const [user, setUser] = useState(auth.user); // useStateを使用
+      {status && <StatusMessage>{status}</StatusMessage>}
 
-    useEffect(() => {
-        // setAuthアクションをディスパッチ
-        dispatch(setAuth({ user: 'satoshi' }));
-    }, [dispatch]);
+      <LoginForm onSubmit={submit} id='loginForm'>
+        {/* Input for Email */}
+        <LabelContainer>
+          <InputLabel htmlFor="email" value="Email" />
+          <TextInput
+            id="email"
+            type="email"
+            name="email"
+            value={data.email}
+            autoComplete="username"
+            isFocused={true}
+            onChange={(e) => setData('email', e.target.value)}
+          />
+          <InputError message={errors.email} />
+        </LabelContainer>
 
-    // Reduxの非同期更新を待つためにuseEffect内でユーザーを更新
-    useEffect(() => {
-        setUser(store.getState().auth.user);
-    }, [store.getState().auth.user]);
+        {/* Input for Password */}
+        <LabelContainer>
+          <InputLabel htmlFor="password" value="Password" />
+          <TextInput
+            id="password"
+            type="password"
+            name="password"
+            value={data.password}
+            autoComplete="current-password"
+            onChange={(e) => setData('password', e.target.value)}
+          />
+          <InputError message={errors.password} />
+        </LabelContainer>
 
-    const groupId = 1;
-
-    return (
-        <>
-            <Head title="Welcome" />
-            <WelcomeContainer>
-                <WelcomeContent className="sm:justify-center sm:items-center">
-                    <Header className="sm:fixed sm:top-0 sm:right-0 p-6 text-end">
-                        {!user ? (
-                            <WelcomeLink
-                                href={route('dashboard')}
-                                className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-                            >
-                                Dashboard
-                            </WelcomeLink>
-                        ) : (
-                            <>
-                                <WelcomeLink
-                                    href={route('login')}
-                                    className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-                                >
-                                    Log in
-                                </WelcomeLink>
-
-                                <WelcomeLink
-                                    href={route('register')}
-                                    className="ms-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
-                                >
-                                    Register
-                                </WelcomeLink>
-                            </>
-                        )}
-                    </Header>
-                    <GroupThemeContainer>
-                        <GroupContainer>
-                            <GroupComponent />
-                        </GroupContainer>
-                        <ThemeContainer>
-                            <Routes>
-                                {/* Static sidebar for GroupComponent */}
-                                <Route
-                                    path="/group/:groupId/*"
-                                    element={<GroupComponent />}
-                                />
-
-                                {/* Dynamic main content based on the route */}
-                                <Route
-                                    index
-                                    element={<ThemeComponent groupId={groupId} />}
-                                />
-                                <Route
-                                    path="/theme/:themeId"
-                                    element={<ThreadComponent />}
-                                />
-                            </Routes>
-                        </ThemeContainer>
-                    </GroupThemeContainer>
-                </WelcomeContent>
-            </WelcomeContainer>
-        </>
-    );
+        {/* Checkbox for Remember Me */}
+        <LabelContainer as="div">
+          <Checkbox
+            name="remember"
+            checked={data.remember}
+            onChange={(e) => setData('remember', e.target.checked)}
+          />
+          <PassRem>パスワードを記憶する。</PassRem>
+        </LabelContainer>
+        
+        {/* Login and Forgot Password */}
+        <ButtonContainer>
+          {canResetPassword && (
+            <LinkStyled href={route('password.request')}>
+              パスワードを忘れてしまいましたか？
+            </LinkStyled>
+          )}
+          <PrimaryButton disabled={processing}>Log in</PrimaryButton>
+        </ButtonContainer>
+      </LoginForm>
+    </GuestLayout>
+  );
 }
