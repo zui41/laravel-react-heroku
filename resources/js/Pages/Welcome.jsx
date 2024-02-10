510d89611da7caa0857867a5406133d853add49b
F0gr1 <gami20010314@gmail.com>
2024-02-10 22:42:10 +0900

    update ログイン

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -61,7 +61,30 @@ export default function Welcome({ auth }) {
             <WelcomeContainer>
                 <WelcomeContent className=" sm:justify-center sm:items-center">
                     <Header className="sm:fixed sm:top-0 sm:right-0 p-6 text-end">
-                        {/* Header content */}
+                    {auth.user ? (
+                            <WelcomeLink
+                                href={route('dashboard')}
+                                className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                            >
+                                Dashboard
+                            </WelcomeLink>
+                        ) : (
+                            <>
+                                <WelcomeLink
+                                    href={route('login')}
+                                    className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                                >
+                                    Log in
+                                </WelcomeLink>
+
+                                <WelcomeLink
+                                    href={route('register')}
+                                    className="ms-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                                >
+                                    Register
+                                </WelcomeLink>
+                            </>
+                        )}
                     </Header>
                     <GroupThemeContainer>
                         <GroupContainer>
