87d6ca0601bcb398b93ac861271c2e719feeb4b7
F0gr1 <gami20010314@gmail.com>
2024-02-11 11:48:02 +0900

    update

--- resources/js/Pages/ThemeMain.jsx
+++ resources/js/Pages/ThemeMain.jsx
@@ -1,15 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Link, Head } from '@inertiajs/react';
-import { Route, Routes } from 'react-router-dom';
 import { useEffect, useState } from 'react'; // useStateを追加
 import GroupComponent from '@/Components/Group/GroupComponent';
 import ThemeComponent from '@/Components/Theme/ThemeComponent';
-import ThreadComponent from '@/Components/Thread/ThreadComponent';
 import store from '@/Store/store';
-import { setAuth } from '@/Store/authSlice';
-import { useDispatch } from 'react-redux';
-import { useNavigate } from "react-router-dom";
 import { useLocation } from 'react-router-dom';
 import PostComponent from '@/Components/Post/PostComponent';
 
@@ -22,31 +16,35 @@ const GroupThemeContainer = styled.div`
 
 const GroupContainer = styled.div`
     width: 10%;
-    height: 100vh;
+    height: 100%;
     color: white;
     background-color: #333;
 `;
 
 const HomeContainer = styled.div`
-    width: 80%;
-    height: 100vh;
+    width: 90%;
+    height: 100%;
     color: white;
     overflow-y: auto;
     margin: 0 3%;
 `;
 
 const ThemeContainer = styled.div`
-    min-height: 100vh;
+    height: 100%;
     display: flex;
     justify-content: center;
     align-items: center;
     text-align: center;
+    flex-direction:column;
 `;
 
 const ThemeContent = styled.div`
     width: 100%;
     height: 100%;
-    z-index: 1;
+    display:flex;
+    flex-direction;row;
+    background-color:lightblue;
+    align-items: center;
 `;
 
 
@@ -54,17 +52,16 @@ const Header = styled.div`
     display:flex;
     justify-content: right;
     align-items: flex-start;
-    height: 15%;
+    height: 7%;
     width:100%;
 `;
 
 const PostContainer = styled.div`           
-    color: 333;
+    color:#333;
     width:35%;
 `;
 
 const NameContainer = styled.div`
-    margin-top:1%;
 `;
 
 function ThemeMain() {
@@ -96,29 +93,28 @@ function ThemeMain() {
       }, []);
 
     return (
+        <>
             <ThemeContainer>
-                <ThemeContent className="sm:justify-center sm:items-center">
-                    <Header className="sm:fixed sm:top-0 sm:right-0 p-6 text-end">
-                            <div>
-
-                            </div>
-                            <PostContainer>
-                                <PostComponent/>
-                            </PostContainer>  
-                            <NameContainer>
-                                {user.name}
-                            </NameContainer>    
-                    </Header>
-                    <GroupThemeContainer>
-                        <GroupContainer>
-                            <GroupComponent groups={groups}/>
-                        </GroupContainer>
-                        <HomeContainer>
-                            <ThemeComponent  themes={themes}/>
-                        </HomeContainer>
-                    </GroupThemeContainer>        
-                </ThemeContent>
+                <Header>
+                    <ThemeContent>
+                        <PostContainer>
+                            <PostComponent/>
+                        </PostContainer>  
+                        <NameContainer>
+                            {user.name}
+                        </NameContainer>  
+                    </ThemeContent>  
+                </Header>
+                <GroupThemeContainer>
+                    <GroupContainer>
+                        <GroupComponent groups={groups}/>
+                    </GroupContainer>
+                    <HomeContainer>
+                        <ThemeComponent  themes={themes}/>
+                    </HomeContainer>
+                </GroupThemeContainer>        
             </ThemeContainer>
+        </>
     );
 }
 
--- resources/js/Pages/ThreadMain.jsx
+++ resources/js/Pages/ThreadMain.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Link, Head } from '@inertiajs/react';
-import { Route, Routes } from 'react-router-dom';
 import { useEffect, useState } from 'react'; // useStateを追加
 import GroupComponent from '@/Components/Group/GroupComponent';
 import ThreadComponent from '@/Components/Thread/ThreadComponent';
 import store from '@/Store/store';
-import { setAuth } from '@/Store/authSlice';
-import { useDispatch } from 'react-redux';
-import { useNavigate } from "react-router-dom";
 import { useLocation } from 'react-router-dom';
+import PostComponent from '@/Components/Post/PostComponent';
 
 const GroupThreadContainer = styled.div`
     width: 100%;
@@ -20,43 +16,51 @@ const GroupThreadContainer = styled.div`
 
 const GroupContainer = styled.div`
     width: 10%;
-    height: 100vh;
+    height: 100%;
     color: white;
     background-color: #333;
 `;
 
 const HomeContainer = styled.div`
     width: 90%;
-    height: 100vh;
+    height: 100%;
     color: white;
     overflow-y: auto;
     margin: 0 3%;
 `;
 
 const ThreadContainer = styled.div`
-    min-height: 100vh;
+    height: 100%;
     display: flex;
     justify-content: center;
     align-items: center;
     text-align: center;
+    flex-direction:column;
 `;
 
 const ThreadContent = styled.div`
     width: 100%;
     height: 100%;
-    z-index: 1;
+    display:flex;
+    flex-direction;row;
+    background-color:lightblue;
+    align-items: center;
 `;
 
-const ThreadLink = styled(Link)`
-    font-weight: 600;
-    color: #4a5568;
-    &:hover {
-        color: #2d3748;
-    }
-`;
+
 
 const Header = styled.div`
-    height: 15%;
+display:flex;
+justify-content: right;
+align-items: flex-start;
+height: 7%;
+width:100%;
+`;
+const PostContainer = styled.div`           
+    color:#333;
+    width:35%;
+`;
+const NameContainer = styled.div`
 `;
 
 function ThreadMain() {
@@ -64,7 +68,7 @@ function ThreadMain() {
     const [user, setUser] = useState({}); // useStateを使用
     const [groups , setGroups] = useState([]);
     const [threads , setThreads] = useState([]);
-    const [themeId , setThemeId] = useState({id:1});
+    const [ThreadId , setThreadId] = useState({id:1});
     // Reduxの非同期更新を待つためにuseEffect内でユーザーを更新
     useEffect(() => {
         setUser(store.getState().auth.user);
@@ -73,10 +77,10 @@ function ThreadMain() {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                if(themeId.id > 0){
+                if(ThreadId.id > 0){
                     const response = await axios.post('/api/group/index', store.getState().auth.user);
                     // データをここで取り出すか、別の関数に渡して処理を行います
-                    const ThreadResponse = await axios.post('/api/post/index' , themeId);
+                    const ThreadResponse = await axios.post('/api/post/index' , ThreadId);
                     setGroups(response.data.id);
                     setThreads(ThreadResponse.data);
                 }
@@ -88,21 +92,24 @@ function ThreadMain() {
       }, []);
     return (
             <ThreadContainer>
-                <ThreadContent className="sm:justify-center sm:items-center">
-                    <Header className="sm:fixed sm:top-0 sm:right-0 p-6 text-end">
-                            <div>
-                                {user.name}
-                            </div>      
-                    </Header>
-                    <GroupThreadContainer>
-                        <GroupContainer>
-                            <GroupComponent groups={groups}/>
-                        </GroupContainer>
-                        <HomeContainer>
-                            <ThreadComponent  threads={threads}/>
-                        </HomeContainer>
-                    </GroupThreadContainer>
-                </ThreadContent>
+                <Header>
+                    <ThreadContent>
+                        <PostContainer>
+                            <PostComponent/>
+                        </PostContainer>  
+                        <NameContainer>
+                            {user.name}
+                        </NameContainer>  
+                    </ThreadContent>  
+                </Header>
+                <GroupThreadContainer>
+                    <GroupContainer>
+                        <GroupComponent groups={groups}/>
+                    </GroupContainer>
+                    <HomeContainer>
+                        <ThreadComponent  threads={threads}/>
+                    </HomeContainer>
+                </GroupThreadContainer>
             </ThreadContainer>
     );
 }
